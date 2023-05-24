782ffab92cfc702ae8278b854dc3edc3e7d2d4bf
Tyler Aldrich <tyler.aldrich@architect.io>
2023-05-24 13:40:03 -0400

    Updated provider generator

--- bin/scripts/provider-generator.ts
+++ bin/scripts/provider-generator.ts
@@ -1,16 +1,15 @@
-#!/usr/bin/env ts-node
-import fs from "fs/promises";
-import Mustache from "mustache";
-import path from "path";
-import url from "url";
+import Mustache from 'npm:mustache';
+import * as path from 'https://deno.land/std@0.188.0/path/mod.ts';
 
-const __dirname = new URL(".", import.meta.url).pathname;
-const providersDir = path.join(__dirname, "../../src/@providers/");
+const __dirname = new URL('.', import.meta.url).pathname;
+const providers_dir = path.join(__dirname, '../../src/@providers/');
 
-const allProviders = (await fs.readdir(providersDir, { withFileTypes: true }))
-  // eslint-disable-next-line unicorn/no-await-expression-member
-  .filter((dirent) => dirent.isDirectory())
-  .map((dirent) => dirent.name);
+const all_providers = [];
+for await (const dirEntry of Deno.readDir(providers_dir)) {
+  if (dirEntry.isDirectory) {
+    all_providers.push(dirEntry.name);
+  }
+}
 
 type ProviderTypeFileOptions = {
   provider_list: {
@@ -19,24 +18,23 @@ type ProviderTypeFileOptions = {
   }[];
 };
 
-const providerTypeFileOptions: ProviderTypeFileOptions = {
+const provider_type_file_options: ProviderTypeFileOptions = {
   provider_list: [],
 };
 
-for (const type of allProviders) {
-  providerTypeFileOptions.provider_list.push({
+for (const type of all_providers) {
+  provider_type_file_options.provider_list.push({
     name: type.replace(/-([\dA-Za-z])/g, (g) => g[1].toUpperCase()),
     slug: type,
   });
 }
 
-fs.writeFile(
-  path.join(providersDir, "supported-providers.ts"),
+Deno.writeTextFileSync(
+  path.join(providers_dir, 'supported-providers.ts'),
   Mustache.render(
-    await fs.readFile(
-      path.join(providersDir, "supported-providers.ts.stache"),
-      "utf8",
+    await Deno.readTextFile(
+      path.join(providers_dir, 'supported-providers.ts.stache'),
     ),
-    providerTypeFileOptions,
+    provider_type_file_options,
   ),
 );

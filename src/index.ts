84369263f2c745cf33f868ffa08a42430d17b071
Ryan Cahill <ryan.cahill444@gmail.com>
2023-05-31 10:57:45 -0400

    linting test in workflow

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
 import { Command } from 'cliffy/command/mod.ts';
+import AddAccountCommand from './commands/add/account.ts';
 import BuildCommand from './commands/build.ts';
 import DeployCommand from './commands/deploy.ts';
 import TagCommand from './commands/tag.ts';
-import AddAccountCommand from './commands/add/account.ts';
 
 export default async function arcctl() {
   await new Command()
@@ -12,3 +12,12 @@ export default async function arcctl() {
     .command('add:account', AddAccountCommand)
     .parse(Deno.args);
 }
+
+export * from './@providers/index.ts';
+export * from './@resources/index.ts';
+export * from './cloud-graph/index.ts';
+export * from './component-store/index.ts';
+export * from './components/index.ts';
+export * from './datacenters/index.ts';
+export * from './environments/index.ts';
+export * from './pipeline/index.ts';

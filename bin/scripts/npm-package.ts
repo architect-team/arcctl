c7c2a41123ba92507343ca77ccbd2fbfa556122e
Tyler Aldrich <tyler.aldrich@architect.io>
2023-06-05 16:48:03 -0400

    Updated npm package generation to include schema json files

--- bin/scripts/npm-package.ts
+++ bin/scripts/npm-package.ts
@@ -1,4 +1,5 @@
 import * as path from 'std/path/mod.ts';
+import { walk } from 'std/fs/walk.ts';
 import { build, emptyDir } from 'dnt';
 
 const __dirname = new URL('.', import.meta.url).pathname;
@@ -16,13 +17,35 @@ await build({
     target: 'ES2020',
   },
   shims: {
+    crypto: true,
     deno: true,
+    timers: true,
+    undici: true,
   },
-  // TODO: Should use the info from existing package.json
   package: {
-    name: 'arcctl',
-    version: '0.0.1-rc',
-    description: 'arcctl',
+    name: '@architect-io/arcctl',
+    version: Deno.args[0],
+    description:
+      'arcctl standardizes the interfaces for common cloud resources like VPCs, managed kubernetes clusters, and more, making it easier for developers to create and manage on-demand cloud infrastructure',
+    license: 'Apache-2.0',
+    engines: { node: '>=12.0.0' },
+    homepage: 'https://github.com/architect-team/arcctl',
+    repository: 'architect-team/arcctl',
+    bugs: 'https://github.com/architect-team/arcctl/issues',
   },
-  importMap: path.join(build_dir, '..', 'deno.json'),
+  importMap: path.join(build_dir, '..', 'import_map.json'),
 });
+
+// Copy files from the root dir into npm package.
+await Deno.copyFile('LICENSE.md', path.join(build_dir, 'LICENSE.md'));
+await Deno.copyFile('README.md', path.join(build_dir, 'README.md'));
+
+// Copy all *.schema.json files into the npm package
+for await (const dirEntry of walk(path.join(__dirname, '..', '..', 'src'))) {
+  if (dirEntry.isFile && dirEntry.name.endsWith('.schema.json')) {
+    // Get rid of everything up to  the src/ folder
+    const src_relative_path = dirEntry.path.replace(new RegExp(`.*${path.SEP}src${path.SEP}`), '').split(path.SEP);
+    await Deno.copyFile(dirEntry.path, path.join(build_dir, 'esm', ...src_relative_path));
+    await Deno.copyFile(dirEntry.path, path.join(build_dir, 'script', ...src_relative_path));
+  }
+}

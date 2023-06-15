b44f9cd9b1d264bfdcb15ec17761ed87a53f5f9c
Ryan Cahill <ryan.cahill444@gmail.com>
2023-06-15 12:51:54 -0400

    updates to fix deno lock file and deno conversion

--- src/cdktf-modules/provider-file.ts
+++ src/cdktf-modules/provider-file.ts
@@ -2,7 +2,9 @@ import { Construct } from 'constructs';
 import * as path from 'std/path/mod.ts';
 import { SensitiveFile, SensitiveFileConfig } from './.gen/providers/local/sensitive-file/index.ts';
 
-export const createProviderFileConstructor = (directory: string) =>
+type CreateProviderFileConstructor<T> = new (scope: Construct, id: string, config: SensitiveFileConfig) => T;
+
+export const createProviderFileConstructor = (directory: string): CreateProviderFileConstructor<SensitiveFile> =>
   class extends SensitiveFile {
     constructor(scope: Construct, id: string, config: SensitiveFileConfig) {
       super(scope, id, {

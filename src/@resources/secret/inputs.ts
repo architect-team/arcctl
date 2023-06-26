67d3d9c3b4ce09dc1631cc7e800b023f5245f1fe
David Thor <davidthor@users.noreply.github.com>
2023-06-25 23:46:31 -0400

    feat(variables): Add support for passing in multiple variables to dependencies from multiple components
    * Added support for components to pass values to mergable variables
    
    * Sorted mergable secret data to help tests pass consistently
    
    * Added more test coverage so components can include additional values in merged variables
    
    * Fixed deno formatting
    
    * Added support for shorthand dependency assertion
    
    * Fixed component to use variables key instead of inputs for passing values to deps
    
    * Added support for dynamic values in passed variables
    
    * Merge loops
    
    ---------
    
    Co-authored-by: Michael Muesch <michael.muesch@architect.io>

--- src/@resources/secret/inputs.ts
+++ src/@resources/secret/inputs.ts
@@ -16,8 +16,21 @@ export type SecretInputs = {
 
   /**
    * Whether or not the secret is required
+   * @default false
    */
   required?: boolean;
+
+  /**
+   * Whether or not the data is to be considered sensitive and stripped from logs
+   * @default false
+   */
+  sensitive?: boolean;
+
+  /**
+   * Whether or not to merge the input data from multiple sources into an array of values
+   * @default false
+   */
+  merge?: boolean;
 };
 
 export default SecretInputs;

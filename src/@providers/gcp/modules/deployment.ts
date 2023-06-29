c37add231855401c1feac777aa95164e5c56df09
Tyler Aldrich <tyler.aldrich@architect.io>
2023-06-29 18:06:55 -0400

    Add v0.5 of the gcp serverless datacenter with hardcoded deployment and single container support

--- src/@providers/gcp/modules/deployment.ts
+++ src/@providers/gcp/modules/deployment.ts
@@ -1,6 +1,7 @@
 import { Construct } from 'constructs';
 import { ResourceOutputs } from '../../../@resources/index.ts';
 import { ResourceModule, ResourceModuleOptions } from '../../module.ts';
+import { CloudRunV2ServiceIamBinding } from '../.gen/providers/google/cloud-run-v2-service-iam-binding/index.ts';
 import { CloudRunV2Service } from '../.gen/providers/google/cloud-run-v2-service/index.ts';
 import { ProjectService } from '../.gen/providers/google/project-service/index.ts';
 import { GoogleCloudCredentials } from '../credentials.ts';
@@ -9,6 +10,7 @@ export class GoogleCloudDeploymentModule extends ResourceModule<
   'deployment',
   GoogleCloudCredentials
 > {
+  private access_policy: CloudRunV2ServiceIamBinding;
   private deployment: CloudRunV2Service;
   outputs: ResourceOutputs['deployment'];
 
@@ -19,6 +21,9 @@ export class GoogleCloudDeploymentModule extends ResourceModule<
       ? [
         new ProjectService(this, 'cloud-run-service', {
           service: 'run.googleapis.com',
+        }),
+        new ProjectService(this, 'iam-service', {
+          service: 'iam.googleapis.com',
         }),
       ]
       : [];
@@ -32,11 +37,51 @@ export class GoogleCloudDeploymentModule extends ResourceModule<
       dependsOn: depends_on,
       name: this.inputs?.name || 'deleting',
       location: region,
+      // TODO: Is this necessary?
+      // ingress: 'INGRESS_TRAFFIC_ALL',
       template: {
+        // TODO: If there are multiple exposed ports, need to create multiple containers and one port mapping each
         containers: [{
           image: this.inputs?.image || 'deleting',
+          args: typeof this.inputs?.entrypoint === 'string'
+            ? this.inputs.entrypoint.split(' ')
+            : this.inputs?.entrypoint,
+          command: typeof this.inputs?.command === 'string' ? this.inputs.command.split(' ') : this.inputs?.command,
+          env: Object.entries(this.inputs?.environment || {}).map(([key, value]) => ({
+            name: key,
+            value: String(value),
+          })),
+          // TODO: service has this information currently,
+          // but should be set up here i think
+          ports: this.inputs?.exposed_ports
+            ? this.inputs.exposed_ports.map((ports) => ({
+              containerPort: ports.target_port,
+            }))
+            : [{ containerPort: 8080 }], // TODO(tyler): This is just for testing
+          resources: {
+            limits: {
+              ...(this.inputs?.cpu ? { cpu: String(this.inputs.cpu) } : {}),
+              ...(this.inputs?.memory ? { memory: this.inputs.memory } : {}),
+            },
+          },
+          volumeMounts: (this.inputs?.volume_mounts || []).map((volume) => {
+            return {
+              name: volume.volume,
+              mountPath: volume.mount_path,
+            };
+          }),
         }],
       },
+    });
+
+    this.access_policy = new CloudRunV2ServiceIamBinding(this, 'noauth-policy', {
+      project: this.deployment.project,
+      location: this.deployment.location,
+      name: this.deployment.name,
+      role: 'roles/run.invoker',
+      members: [
+        'allUsers',
+      ],
     });
 
     this.outputs = {

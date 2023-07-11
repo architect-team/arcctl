60e403ae9247266dd330781760a226e1000fd074
Michael Muesch <michael.muesch@architect.io>
2023-07-11 09:56:51 -0400

    Add start of choc automation

---
+++ vendor_dist/choco/tools/chocolateyinstall.ps1
@@ -0,0 +1,7 @@
+﻿$packageName= 'arcctl'
+$version = "v0.0.3"
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url        = "https://github.com/mueschm/denotest2/releases/download/$version/arcctl-windows-$version.exe"
+
+
+Get-ChocolateyWebFile -PackageName 'arcctl' -FileFullPath "$toolsDir/arcctl.exe" -Url "$url"

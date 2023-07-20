17f026fe7d9cc0e46b2f3bf0400d42b6132be135
Ryan Cahill <ryan.cahill444@gmail.com>
2023-07-20 16:36:12 -0400

    testing

--- src/@providers/s3/provider.ts
+++ src/@providers/s3/provider.ts
@@ -22,7 +22,7 @@ export default class S3Provider extends Provider<S3Credentials> {
   public testCredentialsTest(): Promise<string> {
     const client = S3Utils.getS3Client(this.credentials);
     return client.listBuckets().promise().then(() => '****SUCCEEDED').catch((err) =>
-      `****FAILED ${this.credentials.toString()}`
+      `****FAILED ${JSON.stringify(this.credentials.toString())}`
     );
   } // TODO: remove
 }

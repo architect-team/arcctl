1742c296b08924f20879cea26b858cc315168993
Tyler Aldrich <tyler.aldrich@architect.io>
2023-07-25 11:25:50 -0400

    Use proper function name so the loadbalancer works properly

--- src/@providers/gcp/modules/service.ts
+++ src/@providers/gcp/modules/service.ts
@@ -18,7 +18,8 @@ export class GoogleCloudServiceModule extends ResourceModule<'service', GoogleCl
     const namespace = this.inputs?.namespace || 'ns';
     const service_name = namespace + '--' + this.inputs?.name.replaceAll('/', '-') || 'deleting';
     const service_port = this.inputs?.target_port || 80;
-    const function_name = (this.inputs?.target_deployment?.replaceAll('/', '-') || 'deleting') +
+    const function_name = (this.inputs?.namespace || 'ns') +
+      (this.inputs?.target_deployment?.replaceAll('/', '-') || 'deleting') +
       `--${service_port}`;
 
     let region = '';
